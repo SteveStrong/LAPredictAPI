ff3957d60b4fdb971947d647ef15d82517e9f0ba
SteveStrong <sstrong@apprenticesystems.com>
2020-06-10 05:35:34 -0400

    better

--- payload_wrapper.py
+++ payload_wrapper.py
@@ -26,7 +26,7 @@ class PayloadWrapper:
     def headers(self):
         return {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Credentials', 'true',
+        'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
         'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'}
